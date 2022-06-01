9becedb0eb05c9cb38ba43e38958db1dd03ce2a0
Soline Foret <soline.foret2022@campus-eni.fr>
2022-06-01 10:07:25 +0200

    Fixtures: Méthodes addCities, addCampus, addStates, addUsers fonctionnelles.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,6 +5,7 @@ namespace App\DataFixtures;
 use App\Entity\Campus;
 use App\Entity\City;
 use App\Entity\Place;
+use App\Entity\State;
 use App\Entity\Trip;
 use App\Entity\User;
 use App\Repository\CampusRepository;
@@ -17,26 +18,54 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
-    private $hasher;
-    private $faker;
+
+    private UserPasswordHasherInterface $hasher;
+    private Generator $faker;
 
     public function __construct(UserPasswordHasherInterface $hasher)
     {
         $this->hasher = $hasher;
         $this->faker = Factory::create('fr_FR');
-
-
     }
 
     //la méthode load ajoute cities, places, trips, users
     public function load(ObjectManager $manager): void
     {
 
-        //$this->addCities($manager);
-        //$this->addPlaces($manager);
-//        $this->addTrips();
+        $this->addCampus($manager);
+        $this->addCities($manager);
         $this->addUsers($manager);
+        $this->addStates($manager);
+        //$this->addPlaces($manager);
+        //$this->addTrips();
 
+
+    }
+
+    //méthode pour ajouter des campus
+    public function addCampus(ObjectManager $manager)
+    {
+
+        //je crée une liste de campus
+        $campusList = [
+            ['name' => "Rennes"],
+            ['name' => "Nantes"],
+            ['name' => "Quimper"],
+            ['name' => "Niort"],
+        ];
+
+        //je boucle sur chaque élément de ma liste pour créer les campus dans ma BDD
+        foreach ($campusList as $campus) {
+
+            $newCampus = new Campus();
+
+            $newCampus
+                ->setName($campus['name']);
+
+            $manager->persist($newCampus);
+        }
+
+        $manager->flush();
     }
 
     //méthode pour ajouter des villes
@@ -53,6 +82,7 @@ class AppFixtures extends Fixture
 
         //je boucle sur chaque élément de ma liste pour créer les villes dans ma BDD
         foreach ($cities as $city) {
+
             $newCity = new City();
 
             $newCity
@@ -69,6 +99,7 @@ class AppFixtures extends Fixture
     //méthode pour ajouter des lieux
     public function addPlaces(ObjectManager $manager)
     {
+
         //je récupère la liste de ville de ma BDD
         $cities = $manager->getRepository(City::class)->findAll();
 
@@ -95,11 +126,15 @@ class AppFixtures extends Fixture
     //méthode pour ajouter des participants
     public function addUsers(ObjectManager $manager)
     {
+
         $campusList = $manager->getRepository(Campus::class)->findAll();
 
         for ($i = 0; $i <= 10; $i++) {
 
             $user = new User();
+
+            $plainPassword = $this->faker->password;
+            $hashed = $this->hasher->hashPassword($user, $plainPassword);
 
             $user
                 ->setName($this->faker->lastName)
@@ -107,7 +142,7 @@ class AppFixtures extends Fixture
                 ->setPseudo($this->faker->firstName)
                 ->setPhoneNumber($this->faker->phoneNumber)
                 ->setEMail($this->faker->email)
-                ->setPassWord($this->faker->word)
+                ->setPassWord($hashed)
                 ->setActive(1)
                 ->setAdmin(0)
                 ->setCampus($this->faker->randomElement($campusList));
@@ -115,7 +150,90 @@ class AppFixtures extends Fixture
             $manager->persist($user);
         }
 
+        //Je crée 3 users avec password connu
+
+        $user = new User();
+
+        $plainPassword = "lea";
+        $hashed = $this->hasher->hashPassword($user, $plainPassword);
+
+        $user
+            ->setName("Dubois")
+            ->setFirstname("Léa")
+            ->setPseudo("Fluff")
+            ->setPhoneNumber($this->faker->phoneNumber)
+            ->setEMail("lea.dubois@gmail.com")
+            ->setPassWord($hashed)
+            ->setActive(1)
+            ->setAdmin(0)
+            ->setCampus($this->faker->randomElement($campusList));
+
+        $manager->persist($user);
+
+        $user = new User();
+
+        $plainPassword = "sylvaine";
+        $hashed = $this->hasher->hashPassword($user, $plainPassword);
+
+        $user
+            ->setName("Boucard")
+            ->setFirstname("Sylvaine")
+            ->setPseudo("Sly")
+            ->setPhoneNumber($this->faker->phoneNumber)
+            ->setEMail("sylvaine.boucard@gmail.com")
+            ->setPassWord($hashed)
+            ->setActive(1)
+            ->setAdmin(0)
+            ->setCampus($this->faker->randomElement($campusList));
+
+        $manager->persist($user);
+
+        $user = new User();
+
+        $plainPassword = "soline";
+        $hashed = $this->hasher->hashPassword($user, $plainPassword);
+
+        $user
+            ->setName("Foret")
+            ->setFirstname("Soline")
+            ->setPseudo("Soso")
+            ->setPhoneNumber($this->faker->phoneNumber)
+            ->setEMail("soline.foret@gmail.com")
+            ->setPassWord($hashed)
+            ->setActive(1)
+            ->setAdmin(0)
+            ->setCampus($this->faker->randomElement($campusList));
+
+        $manager->persist($user);
+
         $manager->flush();
+    }
+
+    public function addStates(ObjectManager $manager)
+    {
+
+        //je crée une liste d'état
+        $states = [
+            ['description' => "Créée"],
+            ['description' => "Ouverte"],
+            ['description' => "Clôturée"],
+            ['description' => "En cours"],
+            ['description' => "Passée"],
+            ['description' => "Annulée"]
+        ];
+
+        //je boucle sur chaque élément de ma liste pour créer les états dans ma BDD
+        foreach ($states as $state) {
+
+            $newState = new State();
+
+            $newState->setDescription($state['description']);
+
+            $manager->persist($newState);
+        }
+
+        $manager->flush();
+
     }
 
     //méthode pour ajouter des sorties
