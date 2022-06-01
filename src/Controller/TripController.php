7b09afb931464abf132404820731de1a78ac2a19
Soline Foret <soline.foret2022@campus-eni.fr>
2022-06-01 12:19:01 +0200

    ajout formulaire création Trip + affichage formulaire de création Trip

--- src/Controller/TripController.php
+++ src/Controller/TripController.php
@@ -2,17 +2,41 @@
 
 namespace App\Controller;
 
+use App\Entity\Trip;
+use App\Form\TripType;
+use App\Repository\TripRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/trip', name: 'trip_')]
 class TripController extends AbstractController
 {
-    #[Route('/trip', name: 'app_trip')]
-    public function index(): Response
+    #[Route('/create', name: 'create')]
+    public function create(TripRepository $repo, Request $request): Response
     {
-        return $this->render('trip/security.html.twig', [
-            'controller_name' => 'TripController',
-        ]);
+
+        //création d'une nouvelle sortie
+        $trip = new Trip();
+
+        //création du formulaire
+        $tripForm = $this->createForm(TripType::class, $trip);
+        $tripForm->handleRequest($request);
+
+        //traitement du formulaire
+        if ($tripForm->isSubmitted() && $tripForm->isValid()) {
+
+            $repo->add($trip, true);
+            $this->addFlash("success", "Ta proposition de sortie est ajoutée!");
+            return $this->redirectToRoute();
+
+        }
+
+        return $this->render('trip/create.html.twig',
+            ['tripForm' => $tripForm->createView()]);
+
     }
+
+
 }
