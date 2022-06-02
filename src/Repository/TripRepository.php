efafa26e8ba5e88f7b4601eaa3cacfed65b73fab
Léa <lea.dubois2022@campus-eni.fr>
2022-06-02 17:00:08 +0200

    Commit avant pull màj Sylvaine s'inscrire et annuler

--- src/Repository/TripRepository.php
+++ src/Repository/TripRepository.php
@@ -42,17 +42,14 @@ class TripRepository extends ServiceEntityRepository
 //    /**
 //     * @return Trip[] Returns an array of Trip objects
 //     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('t')
-//            ->andWhere('t.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('t.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+   public function filterBy(): array
+   {
+       return $this->createQueryBuilder('t')
+          ->orderBy('t.name', 'ASC')
+            ->getQuery()
+           ->getResult()
+       ;
+    }
 
 //    public function findOneBySomeField($value): ?Trip
 //    {
