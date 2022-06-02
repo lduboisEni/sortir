d91f54af5d92d606be2c9a864fc5946aec216891
Sylvaine Boucard <sylvaine.boucard@yahoo.fr>
2022-06-02 16:53:48 +0200

    ajout pour test de upload image dans le form edit ajout des fonctions subscribe et cancelSubscribe dans le TripController.php

--- src/Controller/TripController.php
+++ src/Controller/TripController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Form\model\Search;
 use App\Form\SearchType;
 use App\Entity\State;
@@ -11,6 +12,7 @@ use App\Form\TripType;
 use App\Repository\PlaceRepository;
 use App\Repository\StateRepository;
 use App\Repository\TripRepository;
+use App\Repository\UserRepository;
 use Psr\Container\ContainerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -70,20 +72,14 @@ class TripController extends AbstractController
         public function index(Request $request, TripRepository $tripRepository): Response
         {
             $search = new Search();
-            dump('1');
 
             $searchForm =$this->createForm(SearchType::class, $search);
             $searchForm->handleRequest($request);
 
-            dump('2');
-
             $tripList =$tripRepository->findAll();
-
-            dump($tripList);
 
         if ($searchForm->isSubmitted() && $searchForm->isValid()){
 
-            dump('3');
             }
 
            return $this->render('trip/home.html.twig', [
@@ -95,7 +91,6 @@ class TripController extends AbstractController
     #[Route('/display/{id}', name: 'display')]
     public function display($id, TripRepository $tripRepository): Response
     {
-
         $trip = $tripRepository->find($id);
         return $this->render('trip/display.html.twig', [
             'id' => $id,
@@ -103,5 +98,33 @@ class TripController extends AbstractController
         ]);
     }
 
+    public function subscribe($idTrip, TripRepository $tripRepository)
+    {
+        //je récupère mon user en cours
+        $user = $this->getUser();
+        //je récupère la sortie qui a été choisie
+        $trip = $tripRepository->findOneBy($idTrip);
+
+        $trip->getUsers()->add($user);
+        $tripRepository->add($trip, true);
+
+        $this->addFlash("success", "Félicitations vous êtes inscrit !");
+
+    }
+
+    public function cancelSubscribe($idTrip, TripRepository $tripRepository)
+    {
+        //je récupère mon user en cours
+        $user = $this->getUser();
+        //je récupère la sortie qui a été choisie
+        $trip = $tripRepository->findOneBy($idTrip);
+
+        //enlever le participant de la liste
+        //mise à jour de la bdd
+        $trip->getUsers()->remove($user);
+        $tripRepository->add($trip, true);
+
+        $this->addFlash("success", "Vous êtes bien désinscrit !");
+    }
 
 }
