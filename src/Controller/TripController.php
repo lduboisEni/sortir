e7ddecce0cc581e5beb5b999324d9db69369d0fc
Soline Foret <soline.foret2022@campus-eni.fr>
2022-06-07 14:45:59 +0200

    Modification gestion des sorties

--- src/Controller/TripController.php
+++ src/Controller/TripController.php
@@ -33,29 +33,35 @@ class TripController extends AbstractController
         //je récupère la date du jour
         $now = new \DateTime('now');
 
-        foreach ($allTrips as $trip){
-            //je calcule la dateTime à laquelle se termine la sortie (date de début + durée grâce à la fonction modify())
-            $endTimeTrip = $trip->getStartTime()->modify('+'.$trip->getLenght().'minute');
 
-            if ($trip->getRegistrationTimeLimit()<$now) {
-                $state = $stateRepository->findOneBy(array('description'=>"Clôturée"));
-                $trip->setState($state);
-                $tripRepository->add($trip,true);
-            }
-            if ($trip->getStartTime()<=$now && $endTimeTrip>=$now){
-                $state = $stateRepository->findOneBy(array('description'=>"En cours"));
-                $trip->setState($state);
-                $tripRepository->add($trip,true);
-            }
-            if ($endTimeTrip<$now){
-                $state = $stateRepository->findOneBy(array('description'=>"Terminée"));
-                $trip->setState($state);
-                $tripRepository->add($trip,true);
-            }
-            if ($endTimeTrip->modify('+1 month')<=$now){
-                $state = $stateRepository->findOneBy(array('description'=>"Historisée"));
-                $trip->setState($state);
-                $tripRepository->add($trip,true);
+        foreach ($allTrips as $trip) {
+            if ($trip->getState()->getDescription() != "Créée" && $trip->getState()->getDescription() != "Ouverte") {
+                //je calcule la dateTime à laquelle se termine la sortie (date de début + durée grâce à la fonction modify())
+                $endTimeTrip = $trip->getStartTime()->modify('+' . $trip->getLenght() . 'minute');
+                dump("1");
+                if ($trip->getRegistrationTimeLimit() < $now) {
+                    $state = $stateRepository->findOneBy(array('description' => "Clôturée"));
+                    $trip->setState($state);
+                    $tripRepository->add($trip, true);
+                }
+                dump("2");
+                if ($trip->getStartTime() <= $now && $endTimeTrip <= $now) {
+                    $state = $stateRepository->findOneBy(array('description' => "En cours"));
+                    $trip->setState($state);
+                    $tripRepository->add($trip, true);
+                }
+                dump("3");
+                if ($endTimeTrip < $now) {
+                    $state = $stateRepository->findOneBy(array('description' => "Terminée"));
+                    $trip->setState($state);
+                    $tripRepository->add($trip, true);
+                }
+                dump("4");
+                if ($endTimeTrip->modify('+1 month') >= $now) {
+                    $state = $stateRepository->findOneBy(array('description' => "Historisée"));
+                    $trip->setState($state);
+                    $tripRepository->add($trip, true);
+                }
             }
         }
 
